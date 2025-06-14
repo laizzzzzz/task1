3586e3db417fb3a6b036ce5dc1f049f0d3430ce6
Ge|ェ <116347807+laizzzzzz@users.noreply.github.com>
2025-06-14 01:59:36 +0000

    cls

--- mainBasic.java
+++ mainBasic.java
@@ -1,6 +1,31 @@
+
+
+/**
+ * this is a cample class for CIICC b3
+ */
+
 public class mainBasic {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+    //this is a single line comment
+    /*
+     * this is a multiline keneme
+     */
+    
     }
+}
+
+class Car{
+    int year;
+    String name;
+    String color;
+
+}
+
+class Basic2{
+    mainBasic mb;
+    Car carl = new Car();
+    Car ako = new Car();
 }
